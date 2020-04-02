d04908e2109cda78330aace4f36e0fca0969fbf0
waleed-max <36965138+waleed-max@users.noreply.github.com>
2020-04-02 01:36:34 -0500

    add animation for services

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,107 @@
 import React from 'react'
 
-export default function Services() {
-    return (
-       <h1>hello from services page</h1>
-    )
+import Grid from '@material-ui/core/Grid';
+import Switch from '@material-ui/core/Switch';
+import Paper from '@material-ui/core/Paper';
+import Slide from '@material-ui/core/Slide';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    height: 280,
+  },
+  
+  paper: {
+    zIndex: 1,
+    position: 'relative',
+    textAlign: 'center',
+    margin: theme.spacing(1),
+  },
+  svg: {
+    width: 500,
+    height: 175,
+  },
+  polygon: {
+    fill: theme.palette.common.white,
+    stroke: theme.palette.divider,
+    strokeWidth: 1,
+  },
+}));
+
+export default function Service() {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState(false);
+
+  
+  window.onload = function (){
+
+ setChecked((prev) => !prev);
+  }
+
+  return (
+
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          
+          <div className={classes.root}>
+      <div className={classes.wrapper}>
+        
+        <Slide direction="up" in={checked} mountOnEnter unmountOnExit style={{ transformOrigin: '0 0 0' }}
+          {...(checked ? { timeout: 1000 } : {})}>
+          <Paper elevation={4} className={classes.paper}>
+          <p>service 1</p>
+            <svg className={classes.svg}>
+                
+              <polygon  className={classes.polygon} />
+            </svg>
+          </Paper>
+        </Slide>
+      </div>
+    </div>
+
+          
+        </Grid>
+        <Grid item xs={4}>
+          
+          <div className={classes.root}>
+      <div className={classes.wrapper}>
+        
+        <Slide direction="up" in={checked} mountOnEnter unmountOnExit style={{ transformOrigin: '0 0 0' }}
+          {...(checked ? { timeout: 1000 } : {})}>
+          <Paper elevation={4} className={classes.paper} >
+          <p>service 2</p>
+            <svg className={classes.svg}>
+              <polygon  className={classes.polygon} />
+            </svg>
+          </Paper>
+        </Slide>
+      </div>
+    </div>
+          
+        </Grid>
+        <Grid item xs={4}>
+          
+          <div className={classes.root}>
+      <div className={classes.wrapper}>
+        
+        <Slide direction="up" in={checked} mountOnEnter unmountOnExit style={{ transformOrigin: '0 0 0' }}
+          {...(checked ? { timeout: 1000 } : {})}>
+          <Paper elevation={4} className={classes.paper}>
+          <p>service 3</p>
+            <svg className={classes.svg}>
+              <polygon  className={classes.polygon} />
+            </svg>
+          </Paper>
+        </Slide>
+      </div>
+    </div>
+          
+        </Grid>
+      </Grid>
+    </div>
+    
+  );
 }
