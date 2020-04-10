0d36b5b848fab530941d9aaf789d3a308baf7e0d
Mohammed Faisal <59305128+mo-F93@users.noreply.github.com>
2020-04-11 01:12:13 +0300

    finsh services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import ServicesCards from '../Components/ServicesCards/ServicesCards';
-import {Media} from 'react-bootstrap'
+import {Media, Image} from 'react-bootstrap'
 
 export default function services() {
   return (
@@ -35,26 +35,55 @@ export default function services() {
          <div className="ServicesSection">
            
          <Media className="mediaServices">
-  <img
-    
-    className="mr-3"
-    src={require('../assets/SectionServices.jpg')}
-    alt="Generic placeholder"
-  />
-  <Media.Body className="SectionText">
-    <h5>Media Heading</h5>
-    <p>
-      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-      ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-      tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
-      Donec lacinia congue felis in faucibus.
-    </p>
-    <Link to="/">Contact us</Link>
-  </Media.Body>
-</Media>
+         <img
+         className="mr-3"
+         src={require('../assets/SectionServices.jpg')}
+         alt="Generic placeholder"
+         />
+         <Media.Body className="SectionText">
+         <h5>Media Heading</h5>
+         <p>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+          ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+          tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+          Donec lacinia congue felis in faucibus.
+         </p>
+         <Link to="/">Contact us</Link>
+         </Media.Body>
+         </Media>
 
          </div>
-                 
+         
+         <div className="ServicesSection2">
+           
+         <Media className="mediaServices">
+         <Media.Body className="SectionText">
+         <h5>Media Heading</h5>
+         <p>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+          ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+          tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+          Donec lacinia congue felis in faucibus.
+         </p>
+         <Link to="/">See Our Templates</Link>
+         </Media.Body>
+
+         <img
+         className="mr-3"
+         src={require('../assets/SectionServices2.jpg')}
+         alt="Generic placeholder"
+         />
+         
+         </Media>
+
+         </div>
+         
+         <div className="ImageSrvices">
+
+         <Image src={require('../assets/imageSection1.jpg')} fluid />
+
+         </div>
+        
       </div>  
   )
 }
