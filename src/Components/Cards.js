4f6583b608eee1fcc4dec4d5f11c81eb37ebcf55
waleed-max <36965138+waleed-max@users.noreply.github.com>
2020-04-12 02:02:05 -0500

    Add new cards and Footer

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Card, Container, Row, Col} from 'react-bootstrap';
-import '../index.css'
+import '../index.css';
+import { ParallaxButton, ParallaxWrapper} from 'react-parallax-button'
 
 
 export default function Cards() {
@@ -10,53 +11,106 @@ export default function Cards() {
   <Row>
     <Col>
     <Card style={{ width: 'auto' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+    <ParallaxWrapper parallaxScale={0.50}>  
+    <Card.Img variant="top" src="https://image.freepik.com/free-vector/isometric-people-working-with-technology_52683-19078.jpg" />
+</ParallaxWrapper>
+  
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <center><ParallaxButton
+  text="Click Me"
+  backgroundStyle={{
+    background: 'linear-gradient(135deg, #7F00FF, #E100FF)',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, .3)'
+  }}
+  textStyle={{
+    padding: '1.5em 2.5em 1.5em 2.5em',
+    color: 'white'
+  }}
+/></center>
   </Card.Body>
 </Card>
     </Col>
     <Col>
     <Card style={{ width: 'auto' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+    <ParallaxWrapper parallaxScale={0.50}>
+  <Card.Img variant="top" src="https://image.freepik.com/free-vector/isometric-people-working-with-technology_52683-19080.jpg" />
+  </ParallaxWrapper>
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <center><ParallaxButton
+  text="Click Me"
+  backgroundStyle={{
+    background: 'linear-gradient(135deg, #7F00FF, #E100FF)',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, .3)'
+  }}
+  textStyle={{
+    padding: '1.5em 2.5em 1.5em 2.5em',
+    color: 'white'
+  }}
+/></center>
   </Card.Body>
 </Card>
     </Col>
     <Col>
     <Card style={{ width: 'auto' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+    <ParallaxWrapper parallaxScale={0.50}>
+  <Card.Img variant="top" src="https://image.freepik.com/free-vector/isometric-people-working-with-technology_52683-19082.jpg" />
+  </ParallaxWrapper>
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <center><ParallaxButton
+  text="Click Me"
+  backgroundStyle={{
+    background: 'linear-gradient(135deg, #7F00FF, #E100FF)',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, .3)'
+  }}
+  textStyle={{
+    padding: '1.5em 2.5em 1.5em 2.5em',
+    color: 'white'
+  }}
+/></center>
   </Card.Body>
 </Card>
     </Col>
     <Col>
     <Card style={{ width: 'auto' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+    <ParallaxWrapper parallaxScale={0.50}>
+  <Card.Img variant="top" src="https://image.freepik.com/free-vector/isometric-people-working-with-technology_52683-19084.jpg" />
+  </ParallaxWrapper>
   <Card.Body>
     <Card.Title>Card Title</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <center><ParallaxButton
+  text="Click Me"
+  backgroundStyle={{
+    background: 'linear-gradient(135deg, #7F00FF, #E100FF)',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, .3)'
+  }}
+  textStyle={{
+    padding: '1.5em 2.5em 1.5em 2.5em',
+    color: 'white'
+  }}
+/></center>
   </Card.Body>
 </Card>
     </Col>
