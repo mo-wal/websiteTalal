0a3b0afdd15945f7762637ee278000f9a225b092
Mohammed Faisal <59305128+mo-F93@users.noreply.github.com>
2020-05-06 10:15:17 +0300

    fix the scroll to top for react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 
 //imports pages
 import Home from './pages/home';
@@ -19,7 +20,9 @@ function App() {
   return (
    <>
   <Router>
+  <ScrollToTop />
     <Navbar/>
+    <br></br>
     <br></br>
     <br></br>
   <Switch>
